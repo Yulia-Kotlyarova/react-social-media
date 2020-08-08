19c6ecb1aef955fa207ee1fea5f28d72434b7cc3
Yulia Kotlyarova <yuliakykyshka@gmail.com>
2020-08-08 20:43:46 +0300

    Updates

---
+++ precache-manifest.62e548e5ad3f227b9e56ac73174dfd01.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edf7a4fcda48f2def0bc6c4e85e03014",
+    "url": "/react-social-media/index.html"
+  },
+  {
+    "revision": "a03f7e99b6cc8b9c8409",
+    "url": "/react-social-media/static/css/main.97df01b1.chunk.css"
+  },
+  {
+    "revision": "41a25f00992a9e7918bd",
+    "url": "/react-social-media/static/js/2.aec4f7a5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-social-media/static/js/2.aec4f7a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a03f7e99b6cc8b9c8409",
+    "url": "/react-social-media/static/js/main.ddd32667.chunk.js"
+  },
+  {
+    "revision": "df515b0f9b9ae0e25770",
+    "url": "/react-social-media/static/js/runtime-main.3fb44a4e.js"
+  },
+  {
+    "revision": "f906da7a4b5ab0588c7bb3e21a63e4c7",
+    "url": "/react-social-media/static/media/alien.f906da7a.svg"
+  },
+  {
+    "revision": "eeb2faff09097bb8ccbfc236099cc493",
+    "url": "/react-social-media/static/media/angry-monster.eeb2faff.svg"
+  },
+  {
+    "revision": "4c14b0afa8c8cd57d8d1ad79b30c6667",
+    "url": "/react-social-media/static/media/astronaut.4c14b0af.svg"
+  },
+  {
+    "revision": "dff78f241f93b407c2c2c0fb4a5f9a8c",
+    "url": "/react-social-media/static/media/cat-face.dff78f24.svg"
+  },
+  {
+    "revision": "4c00f8702862ad226c6a91c9f934722f",
+    "url": "/react-social-media/static/media/eye-open.4c00f870.svg"
+  },
+  {
+    "revision": "fbc660d0758e5caf3679c64a3be8c8bc",
+    "url": "/react-social-media/static/media/jellyfish.fbc660d0.svg"
+  },
+  {
+    "revision": "ef5728950fa01ea57b1971e9463d3c95",
+    "url": "/react-social-media/static/media/logo.ef572895.svg"
+  },
+  {
+    "revision": "22bc2664fcae3828bbc22281228c5b02",
+    "url": "/react-social-media/static/media/unity-hand.22bc2664.svg"
+  }
+]);
