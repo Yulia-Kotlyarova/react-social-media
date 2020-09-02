429728ff81d3825ce08e909926a4b0f5dc8b281a
Yulia Kotlyarova <yuliakykyshka@gmail.com>
2020-09-02 16:52:02 +0300

    feat: add and use axios

--- src/components/search/SearchUsers.jsx
+++ src/components/search/SearchUsers.jsx
@@ -1,28 +1,33 @@
 import React from 'react';
 import User from './User';
 import cat from '../../img/cat-face.svg';
+import * as axios from 'axios';
 
 const SearchUsers = (props) => {
     debugger
     if (props.users.length === 0) {
-        props.setUsers(        
-            [     
-            {id: 1, followed: false, photoUrl: `${cat}`, fullName: "pipetka", status: "full of pipetochnost", location: { city:'SPb', cnt:'Russia' }},
-            {id: 2, followed: true, photoUrl: `${cat}`, fullName: "meaw", status: "mr cat", location: { city:'SPb', cnt:'Russia' }},
-            {id: 3, followed: false, photoUrl: `${cat}`, fullName: "cosmo-dust", status: "full of dust", location: { city:'no', cnt:'no matter' }},
-            {id: 4, followed: true, photoUrl: `${cat}`, fullName: "unity", status: "we are", location: { city:'all of', cnt:'all of' }}
-            ]
-        )
+        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+            props.setUsers(response.data.items);
+        })
+        // props.setUsers(        
+        //     [     
+        //     {id: 1, followed: false, photoUrl: `${cat}`, fullName: "pipetka", status: "full of pipetochnost", location: { city:'SPb', cnt:'Russia' }},
+        //     {id: 2, followed: true, photoUrl: `${cat}`, fullName: "meaw", status: "mr cat", location: { city:'SPb', cnt:'Russia' }},
+        //     {id: 3, followed: false, photoUrl: `${cat}`, fullName: "cosmo-dust", status: "full of dust", location: { city:'no', cnt:'no matter' }},
+        //     {id: 4, followed: true, photoUrl: `${cat}`, fullName: "unity", status: "we are", location: { city:'all of', cnt:'all of' }}
+        //     ]
+        // )
     }
 
     const userItem = props.users.map( u => {
         return <User id = {u.id} 
-         ava = {u.photoUrl}
-         name = {u.fullName} 
+         photos = {u.photos}
+         ava = {cat}
+         name = {u.name} 
          followed = {u.followed}
          status = {u.status}
-         city = {u.location.city}
-         cnt = {u.location.cnt}
+        //  city = {u.location.city}
+        //  cnt = {u.location.cnt}
          key = {u.id}
          follow = {props.follow}
          unfollow = {props.unfollow} /> 
--- src/components/search/User.jsx
+++ src/components/search/User.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 
 const User = (props) => {
+    debugger
     return (
-        <div className = "col-5  d-flex justify-content-center mr-2 mb-2 user-box">
+        <div className = "col-5 mr-2 mb-2 user-box">
                 <div className ="row">
                     <div className="col-5 d-flex flex-column justify-content-between align-items-center">
-                        <img src = {props.ava} className = "ava-friend" />
+                        {/* <img src = {props.ava.small != null ? props.ava.small : props.ava} className = "ava-friend" /> */}
+                        {
+                            props.photos.small != null ? <img src = {props.photos.small} className = "ava-friend" /> : <img src = {props.ava} className = "ava-friend" />
+                        }
                         {
                             props.followed ? <button className = "btn-second" onClick = { () => props.unfollow(props.id)}> unfollow </button>
                             : <button className = "btn-second" onClick = { () => props.follow(props.id)}> follow </button>
@@ -14,7 +18,7 @@ const User = (props) => {
                     <div className="col-7 d-flex flex-column justify-content-between ">
                         <strong> {props.name} </strong> <br/>
                         <span> {props.status} </span>
-                        <span> {props.city}, {props.cnt} </span>
+                        {/* <span> {props.city}, {props.cnt} </span> */}
                     </div>
                 </div>
         </div>
